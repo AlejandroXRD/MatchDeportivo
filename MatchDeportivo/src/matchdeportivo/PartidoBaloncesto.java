2f65109708b165e4013d6a13ecec3b38d1692d89
AlejandroXRD <alejandrojam011229@gmail.com>
2023-01-07 18:51:05 -0500

    matchDeportivo

---
+++ MatchDeportivo/src/matchdeportivo/PartidoBaloncesto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matchdeportivo;
+
+/**
+ *
+ * @author AlejandroXRD
+ */
+public interface PartidoBaloncesto {
+    public void setCestasEquipoLocal(int marcador);
+    public void setCestasEquipoVisitante(int marcador);
+}
